06248509fb2f471c4b12e3856ba7441636006687
mar-ramosesteves <mar.ramosesteves@gmail.com>
2025-08-08 17:58:24 -0300

    Forçando uso do Dockerfile com render.yaml e otimizando build

--- app.py
+++ app.py
@@ -50,4 +50,5 @@ def buscar():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
+    print(f"Starting Flask app on port {port}")
     app.run(debug=False, host='0.0.0.0', port=port)
